fdbc30521be1cc139cc8a06d3981e43dacdd7332
Guilherme Palma <gomes.lmc@gmail.com>
2020-03-19 13:41:54 -0300

    Cria estrutura do projeto

---
+++ src/test/java/com/guiPalma/apivotacao/ApiVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiPalma.apivotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
