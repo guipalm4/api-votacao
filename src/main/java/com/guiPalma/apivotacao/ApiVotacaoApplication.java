3159b82b590a0a3c46867204d14de5c5cc81b086
Guilherme Palma <gomes.lmc@gmail.com>
2020-03-27 12:45:44 -0300

    Adicionada configuracao para o RabbitMQ no heroku

--- src/main/java/com/guiPalma/apivotacao/ApiVotacaoApplication.java
+++ src/main/java/com/guiPalma/apivotacao/ApiVotacaoApplication.java
@@ -1,5 +1,7 @@
 package com.guiPalma.apivotacao;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -7,6 +9,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -17,7 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+import static java.lang.System.getenv;
 import com.guiPalma.apivotacao.endpoint.service.MonitoramentoService;
 import com.guiPalma.apivotacao.thread.MonitoramentoVotacaoThread;
 
@@ -29,13 +32,32 @@ public class ApiVotacaoApplication  implements ApplicationRunner {
     public static final String QUEUE_GENERIC_NAME = "appGenericQueue";
     public static final String QUEUE_SPECIFIC_NAME = "appResultadoQueue";
     public static final String ROUTING_KEY = "messages.key";
+    public static final String CLOUDAMQP_URL = "	amqp://ydvmaaah:yyZL5QU8DjBb8nV_QLAuZo3ih6EtyiF3@orangutan.rmq.cloudamqp.com/ydvmaaah";	
 	
-	@Autowired
+    @Autowired
 	MonitoramentoService monitoramentoService;
 	
 	public static void main(String[] args) {		
 		SpringApplication.run(ApiVotacaoApplication.class, args);
 	}
+	@Bean
+	 public ConnectionFactory connectionFactory() {
+	     final URI rabbitMqUrl;
+	     try {
+	         rabbitMqUrl = new URI(getEnvOrThrow("CLOUDAMQP_URL"));
+	     } catch (URISyntaxException e) {
+	         throw new RuntimeException(e);
+	     }
+
+	     final CachingConnectionFactory factory = new CachingConnectionFactory();
+	     factory.setUsername(rabbitMqUrl.getUserInfo().split(":")[0]);
+	     factory.setPassword(rabbitMqUrl.getUserInfo().split(":")[1]);
+	     factory.setHost(rabbitMqUrl.getHost());
+	     factory.setPort(rabbitMqUrl.getPort());
+	     factory.setVirtualHost(rabbitMqUrl.getPath().substring(1));
+
+	     return factory;
+	 }
 	
 	@Bean
     public TopicExchange appExchange() {
@@ -72,6 +94,14 @@ public class ApiVotacaoApplication  implements ApplicationRunner {
     @Bean
     public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+    
+    private static String getEnvOrThrow(String name) {
+        final String env = getenv(name);
+        if (env == null) {
+            throw new IllegalStateException("Environment variable [" + name + "] is not set.");
+        }
+        return env;
     }
 
 	@Override
